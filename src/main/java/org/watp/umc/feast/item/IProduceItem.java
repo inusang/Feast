fa4af6a94e0ec91a4aaa7d52efe4454a45950dcb
inusang <836690465@qq.com>
2020-10-20 20:04:03 +0800

    Init Commit

--- src/main/java/org/watp/umc/feast/item/IProduceItem.java
+++ src/main/java/org/watp/umc/feast/item/IProduceItem.java
@@ -6,15 +6,12 @@ import net.minecraft.item.Item;
 import net.minecraft.tileentity.TileEntity;
 
 /**
- * use when produced by feast mod machine instead of recipe
+ * use when produced by feast mod machine
  * 
  */
 public interface IProduceItem {
 	Class<? extends TileEntity> produceIn();
 	
-	/**
-	 * use without recipe
-	 */
 	default Map<Item,Integer> consume() {
 		return null;
 	}
