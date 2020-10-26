9117c460bfd308918d38845b72ad20eb55a3914f
inusang <836690465@qq.com>
2020-10-27 01:12:46 +0800

    Init Commit

--- src/main/java/org/watp/umc/feast/client/ClientEventHandler.java
+++ src/main/java/org/watp/umc/feast/client/ClientEventHandler.java
@@ -3,7 +3,6 @@ package org.watp.umc.feast.client;
 import org.watp.umc.feast.Feast;
 import org.watp.umc.feast.client.gui.DairyMachineGuiContainer;
 import org.watp.umc.feast.client.gui.OvenGuiContainer;
-import org.watp.umc.feast.registry.ContainerTypeRegistry;
 
 import net.minecraft.client.gui.ScreenManager;
 import net.minecraftforge.api.distmarker.Dist;
@@ -17,8 +16,8 @@ public class ClientEventHandler {
 	private static class EventHandlers {
 		@SubscribeEvent
 		public static void registerClientGuiFactories(final FMLClientSetupEvent event) {
-			ScreenManager.registerFactory(ContainerTypeRegistry.containerOven.get(),OvenGuiContainer::new);
-			ScreenManager.registerFactory(ContainerTypeRegistry.containerDairyMachine.get(), DairyMachineGuiContainer::new);
+			ScreenManager.registerFactory(Feast.ContainerTypes.OVEN,OvenGuiContainer::new);
+			ScreenManager.registerFactory(Feast.ContainerTypes.DAIRY_MACHINE, DairyMachineGuiContainer::new);
 		}
 	}
 }
