f8cb22fc67f39d60a065ca7e8195b1905c3d6c1f
inusang <836690465@qq.com>
2021-01-23 01:14:22 +0800

    Init Commit

--- src/main/java/org/watp/umc/feast/block/CabbageBlock.java
+++ src/main/java/org/watp/umc/feast/block/CabbageBlock.java
@@ -1,10 +1,18 @@
 package org.watp.umc.feast.block;
 
+import com.google.common.collect.Sets;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 
-public class CabbageBlock extends Block {
+import java.util.Set;
+
+public class CabbageBlock extends Block implements IGenBlock {
 	public CabbageBlock() {
 		super(Properties.create(Material.GOURD));
+	}
+
+	@Override
+	public Set<String> genInBiome() {
+		return Sets.newHashSet();
 	}
 }
--- src/main/java/org/watp/umc/feast/block/SaltOreBlock.java
+++ src/main/java/org/watp/umc/feast/block/SaltOreBlock.java
@@ -1,11 +1,19 @@
 package org.watp.umc.feast.block;
 
+import com.google.common.collect.Sets;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraftforge.common.ToolType;
 
-public class SaltOreBlock extends Block {
+import java.util.Set;
+
+public class SaltOreBlock extends Block implements IGenBlock {
 	public SaltOreBlock() {
 		super(Properties.create(Material.ICE).hardnessAndResistance(1f).harvestTool(ToolType.PICKAXE).harvestLevel(1));
+	}
+
+	@Override
+	public Set<String> genInBiome() {
+		return Sets.newHashSet();
 	}
 }
